02d2c7f599b3f4c02c3978799b3d480fd27a3754
stanly-sudo <alys.git24@gmail.com>
2025-07-02 13:09:39 +0800

    Header and Footer

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <>
+      <footer>
+          <p>&copy; My app</p>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
--- src/Header.jsx
+++ src/Header.jsx
@@ -5,7 +5,17 @@ function Header() {
             <h1>
                 hello world
             </h1>
+            <nav>
+                <ul>
+                    <li><a href="#">home</a> </li>
+                    <li><a href="#">about</a> </li>
+                    <li><a href="#">sevices</a> </li>
+                    <li><a href="#">contact</a> </li>
+                </ul>
+            </nav>
+            <hr />
         </header>
+        
     </>
   )
 }
